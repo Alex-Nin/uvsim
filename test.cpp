3a992bc1b60d53162f501b78dfd9d1aaca9c46e8
Czh025 <zh.chen025@gmail.com>
2024-06-07 17:56:54 -0600

    test cassert

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include "uvsim.h"
 
-void test1() {
+void TestSetMemory() {
     UVSim simulator;
     simulator.setMemory(0, 1001);
 
@@ -11,8 +11,24 @@ void test1() {
     assert(memory[0] == 1001);
 }
 
+void TestLoadProgram() {
+    UVSim simulator;
+    simulator.loadProgram("testFiles/test1.txt");
+
+    auto memory = simulator.getMemory();
+    assert(memory[0] == 1007);
+    assert(memory[1] == 1008);
+    assert(memory[2] == 2007);
+    assert(memory[3] == 2008);
+    assert(memory[4] == 2109);
+    assert(memory[5] == 1109);
+    assert(memory[6] == 4300);
+    assert(memory[7] == 0000);
+}
+
 int main() {
-    test1();
+    TestSetMemory();
+    TestLoadProgram();
 
     std::cout << "All tests passed!" << std::endl;
     return 0;
