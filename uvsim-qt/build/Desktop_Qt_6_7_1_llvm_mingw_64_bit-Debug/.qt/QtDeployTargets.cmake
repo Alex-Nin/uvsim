6ecd027af9524fb1f01d4b66e8c619915379bd93
Jess Smith <10869841@uvu.edu>
2024-06-15 11:28:35 -0600

    uvsim-qt added

---
+++ uvsim-qt/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_uvsim-qt_FILE C:/Users/Jess Smith/src/cs2450-2024/CS2450/CS2450/uvsim-qt/build/Desktop_Qt_6_7_1_llvm_mingw_64_bit-Debug/uvsim-qt.exe)
+set(__QT_DEPLOY_TARGET_uvsim-qt_RUNTIME_DLLS C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/llvm-mingw_64/bin/Qt6Core.dll)
